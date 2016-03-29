d0661d1ffcb6232f7f87346788519b400cda8d48
Dhruti Upender <dhruti.upender@gmail.com>
2016-03-28 22:05:52 -0500

    now outputting to DOM but not using bootstrap colmns

--- quiz.js
+++ quiz.js
@@ -2,15 +2,15 @@
 
 function populatePage(inventory) {
   // Loop over the inventory and populate the page
-      let outputString = "";
+    let outputString = "";
     let container=document.getElementsByClassName("container")
   for (var i = 0; i < inventory.length; i++) {
     console.log("i",i );
     let currentCar = inventory[i];
     console.log("inventory",currentCar );
     outputString +=`<div id="output" class="row"><div class="col-xs-4"><p>${currentCar.year}  ${currentCar.make}  ${currentCar.model}</p><p>Price:  ${currentCar.price}</p>  Color:  ${currentCar.color}  <p>${currentCar.purchased}</p><p>Description:  ${currentCar.description}</p></div></div>`
+    container[0].innerHTML = outputString;
     console.log("outputString",outputString);
-    container[i].innerHTML = outputString;
   };
 
   // Now that the DOM is loaded, establish all the event listeners needed
@@ -20,3 +20,4 @@ function populatePage(inventory) {
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
 CarLot.loadInventory(populatePage);
+// 
