4d95c3003e95da557e1531610bbcf90cf062e4ab
Dhruti Upender <dhruti.upender@gmail.com>
2016-04-03 09:12:13 -0500

    figured out last code was adding border then deleting it again, looking for fix

--- quiz.js
+++ quiz.js
@@ -7,11 +7,12 @@ function populatePage(inventory) {
   for (var i = 0; i < inventory.length; i++) {
     let currentCar = inventory[i];
     // console.log("inventory",currentCar );
-    outputString +=`<div class="col-xs-4"><div id="eachCar"><p>${currentCar.year}  ${currentCar.make}  ${currentCar.model}</p><p>Price:  ${currentCar.price}</p>  Color:  ${currentCar.color}  <p>${currentCar.purchased}</p><p>Description:  ${currentCar.description}</p></div></div>`
+    outputString +=`<div class="col-xs-4"><div id="eachCar${i}"><p>${currentCar.year}  ${currentCar.make}  ${currentCar.model}</p><p>Price:  ${currentCar.price}</p>  Color:  ${currentCar.color}  <p>${currentCar.purchased}</p><p>Description:  ${currentCar.description}</p></div></div>`
+
     output.innerHTML = outputString;
-    if (i===0){
+    if (i===0||1||2){
       console.log("i", i);
-      document.querySelector("#eachCar").classList.add("fireBrick");   
+      document.getElementById('eachCar${[0]}').classList.add("fireBrick");  
     }
   };
 
